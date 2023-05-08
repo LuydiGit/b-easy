1d121ee2cfd6aeb52820b68c91079aba08ede4c0
Luydi Bentes <81865020+LuydiGit@users.noreply.github.com>
2023-05-07 22:51:13 -0300

    Front end top começando

--- app/App.js
+++ app/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Bem vindo ao barber easy!</Text>
+      <Text>Front end Top</Text>
       <StatusBar style="auto" />
     </View>
   );
